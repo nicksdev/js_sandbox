8ad65acaefa5680bc6ccf0cd50e44a2f8b47d762
nick <nick@nickhughes.io>
2018-04-22 22:52:29 +1000

    Arrow Functions

---
+++ app.js
@@ -0,0 +1,13 @@
+// const sayHello = function(){
+//     console.log('Hello');
+// };
+
+
+// const sayHello = () => {
+//     console.log('Hello');
+// };
+
+const sayHello = () => console.log('Hello3');
+
+sayHello();
+
